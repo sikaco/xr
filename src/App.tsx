600c30d6e02a2609f94edff5d7251691d6ca5692
sikaco <sikaco0@gmail.com>
2023-02-06 15:33:02 +0800

    xr demo running successful

--- src/App.tsx
+++ src/App.tsx
@@ -49,35 +49,67 @@ function OriginWelcomePage() {
 function App() {
   const canvas = useRef(null)
 
+  const createCamera = (scene: BABYLON.Scene) => {
+    const alpha = Math.PI / 4
+    const beta = Math.PI / 3
+    const r = 8
+    const target = new BABYLON.Vector3(0, 0, 0)
+
+    const camera = new BABYLON.ArcRotateCamera('Camera', alpha, beta, r, target, scene)
+    camera.attachControl(canvas, true)
+  }
+
+  const createBox = (scene: BABYLON.Scene) => {
+    const box = BABYLON.MeshBuilder.CreateBox('box', {})
+    box.position.x = 0.5
+    box.position.y = 1
+
+    const boxMaterial = new BABYLON.StandardMaterial('material', scene)
+    boxMaterial.diffuseColor = BABYLON.Color3.Random()
+    box.material = boxMaterial
+
+    box.actionManager = new BABYLON.ActionManager(scene)
+    box.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, (event) => {
+      const sourceBox = event.meshUnderPointer
+      if (!sourceBox) {
+        return
+      }
+
+      // move the box upright
+      sourceBox.position.x += 0.1
+      sourceBox.position.y += 0.1
+
+      // update the color
+      boxMaterial.diffuseColor = BABYLON.Color3.Random()
+    }))
+  }
+
+  const createScene = function (engine: BABYLON.Engine) {
+    const scene = new BABYLON.Scene(engine);
+    scene.clearColor = BABYLON.Color3.Black().toColor4();
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
+
+    createCamera(scene)
+    createBox(scene)
+
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 4, height: 4 });
+
+    return scene.createDefaultXRExperienceAsync({
+      floorMeshes: [ground]
+    }).then((xrExperience) => {
+      console.log("Done, WebXR is enabled.");
+      return scene;
+    });
+  }
+
   useEffect(() => {
     const engine = new BABYLON.Engine(canvas.current, true);
 
-    const createScene = function () {
-      const scene = new BABYLON.Scene(engine);
-      scene.clearColor = BABYLON.Color3.Black().toColor4();
+    createScene(engine).then(scene => {
+      engine.runRenderLoop(() => scene.render());
+    })
 
-      const alpha = Math.PI / 4
-      const beta = Math.PI / 3
-      const r = 8
-      const target = new BABYLON.Vector3(0, 0, 0)
 
-      const camera = new BABYLON.ArcRotateCamera('Camera', alpha, beta, r, target, scene)
-      camera.attachControl(canvas, true)
-
-      const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
-
-      const box = BABYLON.MeshBuilder.CreateBox('box', {})
-      box.position.x = 0.5
-      box.position.y = 1
-
-      return scene;
-    }
-
-    const sceneToRender = createScene();
-
-    engine.runRenderLoop(function () {
-      sceneToRender.render();
-    });
   }, [])
 
   return (
